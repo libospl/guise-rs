759e824e553f937a4ac7293c4875d472dcd090d0
Alan Beveridge <adgbeveridge@proton.me>
2023-03-10 15:57:34 -0800

    Add guise c++ code to code files.
    Everything else is untouched.

--- include/blobstore.h
+++ include/blobstore.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "rust/cxx.h"
 #include <memory>
+#include <dlib/dnn.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
 
 namespace org {
 namespace blobstore {
@@ -24,3 +29,54 @@ std::unique_ptr<BlobstoreClient> new_blobstore_client();
 
 } // namespace blobstore
 } // namespace org
+
+
+template <template <int, template <typename> class, int, typename> class block, int N, template <typename> class BN, typename SUBNET>
+using residual = dlib::add_prev1<block<N, BN, 1, dlib::tag1<SUBNET>>>;
+
+template <template <int, template <typename> class, int, typename> class block, int N, template <typename> class BN, typename SUBNET>
+using residual_down = dlib::add_prev2<dlib::avg_pool<2, 2, 2, 2, dlib::skip1<dlib::tag2<block<N, BN, 2, dlib::tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET>
+using block = BN<dlib::con<N, 3, 3, 1, 1, dlib::relu<BN<dlib::con<N, 3, 3, stride, stride, SUBNET>>>>>;
+
+template <int N, typename SUBNET>
+using ares = dlib::relu<residual<block, N, dlib::affine, SUBNET>>;
+template <int N, typename SUBNET>
+using ares_down = dlib::relu<residual_down<block, N, dlib::affine, SUBNET>>;
+
+template <typename SUBNET>
+using alevel0 = ares_down<256, SUBNET>;
+template <typename SUBNET>
+using alevel1 = ares<256, ares<256, ares_down<256, SUBNET>>>;
+template <typename SUBNET>
+using alevel2 = ares<128, ares<128, ares_down<128, SUBNET>>>;
+template <typename SUBNET>
+using alevel3 = ares<64, ares<64, ares<64, ares_down<64, SUBNET>>>>;
+template <typename SUBNET>
+using alevel4 = ares<32, ares<32, ares<32, SUBNET>>>;
+
+using anet_type = dlib::loss_metric<dlib::fc_no_bias<128, dlib::avg_pool_everything<alevel0<alevel1<alevel2<alevel3<alevel4<dlib::max_pool<3, 3, 2, 2, dlib::relu<dlib::affine<dlib::con<32, 7, 7, 2, 2, dlib::input_rgb_image_sized<150>>>>>>>>>>>>>;
+
+namespace guise
+{
+    class Guise
+    {
+    public:
+        Guise(std::string resnet, std::string landmk);
+        ~Guise(){};
+
+        std::map<dlib::rectangle, dlib::rectangle> compare_images(std::string file_one, std::string file_two);
+        bool compare_face_rectangles(std::pair<dlib::rectangle, dlib::rectangle> pair, std::string file_one, std::string file_two);
+
+    private:
+        bool compare_face_rectangles(std::pair<dlib::rectangle, dlib::rectangle> pair, dlib::matrix<dlib::rgb_pixel> img_one, dlib::matrix<dlib::rgb_pixel> img_two);
+        std::vector<dlib::rectangle> get_faces(dlib::matrix<dlib::rgb_pixel> img);
+        bool compare_faces(dlib::matrix<dlib::rgb_pixel> &face_one, dlib::matrix<dlib::rgb_pixel> &face_two);
+
+        // AI.
+        dlib::frontal_face_detector detector;
+        dlib::shape_predictor sp;
+        anet_type net;
+    };
+}
--- src/blobstore.cc
+++ src/blobstore.cc
@@ -69,3 +69,122 @@ std::unique_ptr<BlobstoreClient> new_blobstore_client() {
 
 } // namespace blobstore
 } // namespace org
+
+
+using namespace guise;
+
+Guise::Guise(std::string resnet, std::string landmk)
+{
+    detector = dlib::get_frontal_face_detector();
+    dlib::deserialize(resnet) >> sp;
+    dlib::deserialize(landmk) >> net;
+}
+
+// Get the face rectangle and the shape detection.
+std::vector<dlib::rectangle> Guise::get_faces(dlib::matrix<dlib::rgb_pixel> img)
+{
+    return detector(img);
+}
+
+// Get retangle coordinates of faces that are the same.
+std::map<dlib::rectangle, dlib::rectangle> Guise::compare_images(std::string file_one, std::string file_two)
+{
+    // The first retangle references file_one, the second references file_two.
+    std::map<dlib::rectangle, dlib::rectangle> map;
+    // Vector of faces from the first image.
+    std::vector<dlib::matrix<dlib::rgb_pixel>> faces;
+    // Vector of faces from the second image.
+    std::vector<dlib::matrix<dlib::rgb_pixel>> faces2;
+    // Image storage.
+    dlib::matrix<dlib::rgb_pixel> img;
+    dlib::matrix<dlib::rgb_pixel> img2;
+    // Temporary retangle data.
+    std::vector<dlib::rectangle> tmp;
+
+    // Extract all faces from the first image.
+    load_image(img, file_one);
+    tmp = get_faces(img);
+
+    // Extract all faces from the second image and compare.
+    load_image(img2, file_two);
+    for (auto face : get_faces(img2))
+    {
+        for (long unsigned i = 0; i < tmp.size(); i++)
+        {
+            std::pair<dlib::rectangle, dlib::rectangle> pair;
+            pair.first = tmp[i];
+            pair.second = face;
+            bool res = compare_face_rectangles(pair, img, img2);
+
+            if (res == true)
+            {
+                map[pair.first] = pair.second;
+                std::cout << "Found pair!" << std::endl;
+            }
+        }
+    }
+    return map;
+}
+
+// Compare two rectangles.
+bool Guise::compare_face_rectangles(std::pair<dlib::rectangle, dlib::rectangle> pair, std::string file_one, std::string file_two)
+{
+    dlib::matrix<dlib::rgb_pixel> img;
+    load_image(img, file_one);
+
+    dlib::matrix<dlib::rgb_pixel> img_two;
+    load_image(img_two, file_two);
+
+    return compare_face_rectangles(pair, img, img_two);
+}
+
+// Compare two rectangles. Please pass the whole image.
+bool Guise::compare_face_rectangles(std::pair<dlib::rectangle, dlib::rectangle> pair, dlib::matrix<dlib::rgb_pixel> img_one, dlib::matrix<dlib::rgb_pixel> img_two)
+{
+    // Extract first face chip using rectangle and image.
+    auto val = sp(img_one, pair.first);
+    dlib::matrix<dlib::rgb_pixel> face_chip;
+    extract_image_chip(img_one, get_face_chip_details(val, 150, 0.25), face_chip);
+    face_chip = std::move(face_chip);
+
+    val = sp(img_two, pair.second);
+    dlib::matrix<dlib::rgb_pixel> face_chip2;
+    extract_image_chip(img_two, get_face_chip_details(val, 150, 0.25), face_chip2);
+    face_chip2 = std::move(face_chip2);
+
+    return compare_faces(face_chip, face_chip2);
+}
+
+bool Guise::compare_faces(dlib::matrix<dlib::rgb_pixel> &face_one, dlib::matrix<dlib::rgb_pixel> &face_two)
+{
+    // Convert each face image in faces into a 128D vector.
+    std::vector<dlib::matrix<float, 0, 1>> face_descriptors;
+    face_descriptors.push_back(net(face_one));
+    face_descriptors.push_back(net(face_two));
+
+    // Graph the two faces to see if they are similar enough.
+    std::vector<dlib::sample_pair> edges;
+    for (size_t i = 0; i < face_descriptors.size(); ++i)
+    {
+        for (size_t j = i; j < face_descriptors.size(); ++j)
+        {
+            // If the similarity of the images is close enough, then mark.
+            auto len = length(face_descriptors[i] - face_descriptors[j]);
+            if (len < .6)
+                edges.push_back(dlib::sample_pair(i, j));
+        }
+    }
+
+    // Number of individials may be less and the number of actual faces in the image.
+    std::vector<unsigned long> people; // An array of the two faces identified with an id (a number starting at zero)
+    const auto number_of_individuals = chinese_whispers(edges, people);
+
+    if (number_of_individuals == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
